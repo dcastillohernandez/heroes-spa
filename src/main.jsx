213b5cabd0279b763ee05da65a02a2d106be3044
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-08-18 10:11:49 -0600

    update, adding some comment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // Importamos BrowserRouter de react-router-dom
 
 import { HeroesApp } from './HeroesApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Usamos BrowserRouter para redirigir entre rutas recordar que BrowserRouter es un higher order component */}
+    {/* Siempre poner el BrowserRouter dentro del modo de React.StrictMode */}
     <BrowserRouter>
       <HeroesApp />
     </BrowserRouter>
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -14,9 +14,9 @@ export const Navbar = () => {
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-            
-            <Link 
-                className="navbar-brand" 
+
+            <Link
+                className="navbar-brand"
                 to="/"
             >
                 Asociaciones
@@ -25,26 +25,28 @@ export const Navbar = () => {
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
-                    <NavLink 
-                        className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                    <NavLink
+                    /* De esta forma definimos la clase activa */
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/marvel"
                     >
-                        Marvel
+                        Marvel 
                     </NavLink>
 
-                    <NavLink 
-                         className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                    <NavLink
+                    /* De esta forma definimos la clase activa */
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/dc"
                     >
                         DC
                     </NavLink>
-                    
+
                 </div>
             </div>
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    
+
                     <span className="nav-item nav-link text-primary">
                         Dayron
                     </span>
