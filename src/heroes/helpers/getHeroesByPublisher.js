412e8353208cbe0206d49014bf1f7817f0c81679
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-08-18 09:45:01 -0600

    update

---
+++ src/heroes/helpers/getHeroesByPublisher.js
@@ -0,0 +1,9 @@
+import { heroes } from '../data/heroes';
+
+export const getHeroesByPublisher = (publicher) => {
+    const validPublishers = ['DC Comics', 'Marvel Comics'];
+    if (!validPublishers.includes(publicher)) {
+        throw new Error(`${publicher} is not a valid publisher`);
+    }
+    return heroes.filter(hero => hero.publisher === publicher);
+}
--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -14,11 +14,10 @@ export const HeroesRoutes = () => {
                     <Route path="marvel" element={<MarvelPage />} />
                     <Route path="dc" element={<DcPage />} />
                     <Route path="search" element={<SearchPage />} />
-                    <Route path="hero" element={< HeroPage/>} />
-                    <Route path='*' element={<Navigate to="/marvel" />} />
+                    <Route path="hero" element={< HeroPage />} />
+                    <Route path="*" element={<Navigate to="/marvel" />} />
                 </Routes>
             </div>
-
         </>
     )
 }
