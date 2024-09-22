925b2eceb58fb12e085860b95e6b80c86faeee30
Dayron <dayroncastillohernandez1994@gmail.com>
2024-09-22 19:05:53 -0400

    update, adding some spacing in a Comment in AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -9,9 +9,11 @@ export const AppRouter = () => {
         <>
             <Routes>
                 <Route path="login" element={<LoginPage />} />
+                
                 {/*
                     Cuando no queremos que se muestre algún componente como en este caso que no quiero que en este login se muestre el navbar, creamos otra ruta que redireccione a otro screen o componente donde si tengo establecida todas las rutas con los componentes que quiero que se muestren
                 */}
+                
                 <Route path='*' element={<HeroesRoutes />} />
             </Routes>
            
