911c5d60b9cf8172a131ddb878e3af4d29955258
Dayron <dayroncastillohernandez1994@gmail.com>
2024-09-26 13:46:43 -0400

    update adding some fixing in syntax for this document

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,20 +1,18 @@
-import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher';
+import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher";
 
 export const HeroList = ({ publisher }) => {
-
     const heroes = getHeroesByPublisher(publisher);
-  
-    return (
-    <ul>
 
-        {/* 
+    return (
+        <ul>
+            {/* 
             Cuando usamos heroes.map, retornamos una nueva lista de heroes donde la key es el id del heroe y el value es el superheroe, algo que podemos ver en el data.js
         */}
-        <li>
-            {heroes.map(hero => (
-                <li key={hero.id}>{hero.superhero}</li>
-            ))}
-        </li>
-    </ul>
-  )
-}
+            <li>
+                {heroes.map((hero) => (
+                    <li key={hero.id}>{hero.superhero}</li>
+                ))}
+            </li>
+        </ul>
+    );
+};
