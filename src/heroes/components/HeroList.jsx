f63a0c4bf84a519fca7d34524010c24960494be8
Dayron <dayroncastillohernandez1994@gmail.com>
2024-09-26 13:36:43 -0400

    Adding some comment for explain how implement in this case the map method

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,20 @@
+import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher';
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = getHeroesByPublisher(publisher);
+  
+    return (
+    <ul>
+
+        {/* 
+            Cuando usamos heroes.map, retornamos una nueva lista de heroes donde la key es el id del heroe y el value es el superheroe, algo que podemos ver en el data.js
+        */}
+        <li>
+            {heroes.map(hero => (
+                <li key={hero.id}>{hero.superhero}</li>
+            ))}
+        </li>
+    </ul>
+  )
+}
