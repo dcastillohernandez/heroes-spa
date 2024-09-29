bbb773f3576e618d6400eac8834297845bfc18b5
Dayron <dayroncastillohernandez1994@gmail.com>
2024-09-29 13:46:38 -0400

    adding useParams hook in HeroPage form dynamic URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,11 @@
+import { useParams } from 'react-router-dom';
 
 export const HeroPage = () => {
+
+  // El id viene de la url, este es un parámetro dinámico que definimos anteriormente en el componente HeroesRoutes
+  const { id } = useParams();
+  console.log(id );
+
   return (
     <div>HeroPage</div>
   )
